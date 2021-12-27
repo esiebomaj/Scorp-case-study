92c720153159606ac799a14173b1a71acc635032
JeremAIh <esiebomaj@gmail.com>
2021-12-27 21:13:30 +0100

    refactored into helper functions

--- solutions/Q1.py
+++ solutions/Q1.py
@@ -1,29 +1,37 @@
 from typing import List, NamedTuple
 
-# helper functions for db accesss
-def get_single_post(post_id):
-    # returns a single post with id = post_id
+# helper functions for db accesss in order not to polute the main function with SQL queries
+def get_single_post(post_id:int):
+    """
+    returns a single post with id = post_id
+    """
     sql = f"SELECT * FROM post WHERE id = {post_id}"
     with engine.connect() as connection:
         post = connection.execute(sql)
     return post
 
-def get_post_likes(post_id):
-    # returns all likes for a post with id = post_id
+def get_post_likes(post_id:int):
+    """
+    returns all likes for a post with id = post_id
+    """
     sql = f"SELECT * FROM like WHERE post_id = {post_id}"
     with engine.connect() as connection:
         likes = connection.execute(sql)
-    return likes
+    return likess
 
 def get_post_author(author_id):
-    # returns all likes for a post with id = post_id
+    """
+    returns all likes for a post with id = post_id
+    """
     sql = f"SELECT * FROM user WHERE id = {author_id}"
     with engine.connect() as connection:
         author = connection.execute(sql)
     return author
 
 def get_following_list(user_id:int)->List[int]:
-    # returns list of all users following 
+    """
+    returns list of all users following 
+    """
     sql = f"SELECT following_id FROM follow WHERE follower_id = {user_id}"
     with engine.connect() as connection:
         following = connection.execute(sql)
@@ -45,18 +53,25 @@ def get_posts(user_id: int, post_ids: List[int]) -> List[Post]:
     following_list: List[int] = get_following_list(user_id)
 
     for post_id in post_ids:
-        # query to get single post
+
         post = get_single_post(post_id)
+
         if not post:
-            # append null to post_list
+            # post is does not exist, then we need to append None to post_list and move to the next id
             post_list.append(None)
             continue
 
         # get all likes for the post 
         likes = get_post_likes(post_id)
 
+        # determine if requesting user has liked the post
+        if user_id in likes:
+            post.liked = True
+        else:
+            post.liked = False
+
         # query to get author of each post
-        author = get_post_author()
+        author = get_post_author(post_id)
 
         # determine if the author is followed by requesting user
         # get following list for requesting user 
@@ -67,12 +82,6 @@ def get_posts(user_id: int, post_ids: List[int]) -> List[Post]:
 
         # attach author to coresponding post
         post.owner = author
-
-        # determine if requesting user has liked the post
-        if user_id in likes:
-            post.liked = True
-        else:
-            post.liked = False
 
         # append post to post_list 
         post_list.append(post)
